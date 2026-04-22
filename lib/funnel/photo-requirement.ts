8b01bb5cc5a112436c21cd5da3dff545ef346656
masdjedibelal-dotcom <masdjedibelal@gmail.com>
2026-04-22 23:42:00 +0200

    Delete lib directory

--- lib/funnel/photo-requirement.ts
+++
@@ -1,6 +0,0 @@
-import type { FunnelState } from "./types";
-
-/** Fotos am Lead-Screen sind optional (kein Minimum). */
-export function isBwLeadPhotoRequired(_state: FunnelState): boolean {
-  return false;
-}
