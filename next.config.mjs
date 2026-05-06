7be3f24842d5ef52b6b9b1e4dd4118b8641374d7
masdjedibelal-dotcom <masdjedibelal@gmail.com>
2026-05-06 19:30:47 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,32 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+];
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
