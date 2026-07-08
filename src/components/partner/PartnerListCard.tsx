28d59ab922c112958f4392c1d2928f71ce366d49
masdjedibelal-dotcom <masdjedibelal@gmail.com>
2026-07-08 18:27:55 +0200

    v

--- src/components/partner/PartnerListCard.tsx
+++
@@ -1,9 +0,0 @@
-export {
-  PortalListCard as PartnerListCard,
-  Calendar,
-  Hammer,
-  MapPin,
-  type PortalListCardAccent as PartnerListCardAccent,
-  type PortalListCardMeta as PartnerListCardMeta,
-  type PortalListCardProps as PartnerListCardProps,
-} from "@/components/shared/PortalListCard";
